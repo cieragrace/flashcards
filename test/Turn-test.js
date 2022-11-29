18dae3d834344f23689a3be6011b35d01c07834a
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-29 09:51:57 -0700

    Add both return function tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,27 +1,47 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
+const Turn = require("../src/Turn");
 
-const Turn = require('../src/Card');
 
 describe("Turn", function () {
 
   it('should be a function', function() {
-    const turn = new Turn();
+    let turn = new Turn();
     expect(Turn).to.be.a('function');
-  });
+  })
 
-  it('should be an instance of Card', function() {
-    const turn = new Turn("", {});
+  it('should be an instance of Turn', function() {
+    let turn = new Turn("", {});
     expect(turn).to.be.an.instanceof(Turn);
-  }); 
+  })
 
   it('should store a guess', function() {
-    const turn = new Turn("", {});
+    let turn = new Turn("", {});
     expect(turn.guess).to.deep.equal(turn.guess)
-  }); 
+  })
 
   it('should store card object', function() {
-    const turn = new Turn("", {});
+    let card = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
+    let turn = new Turn("", card);
     expect(turn.cardObj).to.deep.equal(turn.cardObj)
-  }); 
+  })
+
+  it('should return the guess', function() {
+    let card = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
+    let turn = new Turn("accessor method", card);
+    
+    var guessToReturn = turn.returnGuess()
+    
+    expect(guessToReturn).to.equal(turn.guess);
+  })
+
+  it('should return the card', function() {
+    let card = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
+    let turn = new Turn("iteration method", card)
+
+    var cardToReturn = turn.returnCard()
+
+    expect(cardToReturn).to.equal(turn.cardObj)
+  })
 })
