602d13015ca113aceece27b32a1eec5357367c29
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-28 18:01:08 -0700

    Add tests for guess and card object

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,9 +9,19 @@ describe("Turn", function () {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
-})
 
-it('should be an instance of Card', function() {
-  const turn = new Turn("", {});
-  expect(turn).to.be.an.instanceof(Turn);
-}); 
+  it('should be an instance of Card', function() {
+    const turn = new Turn("", {});
+    expect(turn).to.be.an.instanceof(Turn);
+  }); 
+
+  it('should store a guess', function() {
+    const turn = new Turn("", {});
+    expect(turn.guess).to.deep.equal(turn.guess)
+  }); 
+
+  it('should store card object', function() {
+    const turn = new Turn("", {});
+    expect(turn.cardObj).to.deep.equal(turn.cardObj)
+  }); 
+})
