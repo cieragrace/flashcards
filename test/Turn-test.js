d3d5349692069973ea315a947569931d223f047c
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-11-28 17:16:13 -0700

    Add describe for Turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Card');
+
+describe("Turn", function () {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+})
