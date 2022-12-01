5af73ad0fd2df9ef0c35c2965427063e0171efa0
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-12-01 10:06:45 -0700

    Refactor tests to work with refactored method

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,9 +7,12 @@ const Round = require('../src/Round');
 
 
 describe('Round', function() {
-
+//   let card1;
+//   let card2;
+// beforeEach(() =>{
+// card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter')
+})
   it('should be a function', function() {
-    let round = new Round();
     expect(Round).to.be.a('function');
   })
 
@@ -30,12 +33,13 @@ describe('Round', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-
+    let turn = new Turn('sea otter', card1)
+    let turn2 = new Turn('spleen', card2)
     const deck = new Deck([card1, card2, card3])
     let round = new Round(deck)
 
-    round.takeTurn()
-    round.takeTurn()
+    round.takeTurn(turn.guess)
+    round.takeTurn(turn2.guess)
 
     expect(round.turns).to.equal(2)
   })
@@ -44,12 +48,12 @@ describe('Round', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    
+  
     const deck = new Deck([card1, card2, card3])
     let round = new Round(deck)
     let turn = new Turn('sea otter', card1)
 
-    round.takeTurn()
+    round.takeTurn(turn.guess)
 
     expect(turn.guess).to.equal(card1.correctAnswer)
   })
@@ -61,12 +65,32 @@ describe('Round', function() {
     
     const deck = new Deck([card1, card2, card3])
     let round = new Round(deck)
-    let turn = new Turn('sea otter', card1)
+    let turn = new Turn('capybara', card1)
+    console.log(round)
+    round.takeTurn(turn.guess)
 
-    round.takeTurn()
 
     expect(turn.guess).to.not.equal(card3.correctAnswer)
   })
+
+  it('should store incorrect answers', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    
+    const deck = new Deck([card1, card2, card3])
+    let round = new Round(deck)
+    // let turn = new Turn('pug', card1)
+
+    // expect(round.incorrectGuesses).to.be.an('array').that.is.empty
+    
+    round.takeTurn('pug', card1)
+
+    expect(round.incorrectGuesses).to.eql([card1.id])
+
+
+  })
+})
 
   // it('should return Correct if guess and correct answer match', function() {
   //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
@@ -80,4 +104,3 @@ describe('Round', function() {
     
   //   expect(round.takeTurn()).to.equal("Correct")
   // })
-})
