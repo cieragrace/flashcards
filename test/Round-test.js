a4cb6bafc8e2b45d9d57daf9640efa7051f2b51f
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2022-12-01 13:07:23 -0700

    Add tests for feedback

--- test/Round-test.js
+++ test/Round-test.js
@@ -80,27 +80,34 @@ describe('Round', function() {
     
     const deck = new Deck([card1, card2, card3])
     let round = new Round(deck)
-    // let turn = new Turn('pug', card1)
-
-    // expect(round.incorrectGuesses).to.be.an('array').that.is.empty
     
     round.takeTurn('pug', card1)
 
     expect(round.incorrectGuesses).to.eql([card1.id])
-
-
   })
-})
 
-  // it('should return Correct if guess and correct answer match', function() {
-  //   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  //   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  //   const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-  //   const deck = new Deck([card1, card2, card3])
-  //   let round = new Round(deck)
-  //   let turn = new Turn('sea otter', card1)
+  it('should return Correct if guess and correct answer match', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3])
+    let round = new Round(deck)
+    let turn = new Turn('sea otter', card1)
 
-  //   round.takeTurn()
+    round.takeTurn(turn.guess)
     
-  //   expect(round.takeTurn()).to.equal("Correct")
-  // })
+    expect(turn.giveFeedback()).to.equal("Correct")
+  })
+
+  it('should return Incorrect if guess and correct answer do not match', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3])
+    let round = new Round(deck)
+    let turn = new Turn('pug', card1)
+
+    round.takeTurn(turn.guess)
+    
+    expect(turn.giveFeedback()).to.equal("Incorrect")
+  })
